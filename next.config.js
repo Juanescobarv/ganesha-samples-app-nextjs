ff8fd931f20af2551e023280da60b66052ce65eb
“CristiAndLook” <krhistian1902@gmail.com>
2023-07-28 19:04:32 -0500

    New deploy try

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
+    assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || '',
+}
 
 
 module.exports = nextConfig
