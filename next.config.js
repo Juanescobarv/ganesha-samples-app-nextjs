14c9f0441261a763795c2f49450ac45a2e2fa90d
“CristiAndLook” <krhistian1902@gmail.com>
2023-07-28 19:14:20 -0500

    Try deploy

--- next.config.js
+++ next.config.js
@@ -2,6 +2,8 @@
 const nextConfig = {
     basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
     assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || '',
+    output: 'export',
+    distDir: 'dist',
 }
 
 
