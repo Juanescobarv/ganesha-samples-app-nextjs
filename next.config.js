bf1e55b175d5b8c705b2fc5d38c97276d0d8164d
Cristian Andres MQ <64241547+CristiAndLook@users.noreply.github.com>
2023-07-31 20:10:01 +0000

    FIX code

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
-    assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || '',
-    output: 'export',
-    distDir: 'out',
+    // Your config
 }
 
 
