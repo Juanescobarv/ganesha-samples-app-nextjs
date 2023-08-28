692970ab64458368fb32e2d5db1e601d9266ae57
“CristiAndLook” <krhistian1902@gmail.com>
2023-08-27 23:42:01 -0500

    Spinner color #EE2B7B

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -2,11 +2,8 @@ export function Spinner() {
     return (
         <div className="flex justify-center items-center h-10">
             <div
-                className="inline-block h-10 w-10 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
+                className="inline-block h-10 w-10 animate-spin rounded-full border-[#EE2B7B] border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
                 role="status">
-                <span
-                    className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]"
-                ></span>
             </div>
         </div>
     )
