1ff372aaa4e3633cddbd80b298bcff3430daaf59
“CristiAndLook” <krhistian1902@gmail.com>
2023-08-31 00:20:16 -0500

    Component Main, Grid

--- src/components/options/NotOption.jsx
+++ src/components/options/NotOption.jsx
@@ -2,19 +2,31 @@ export function NotOption() {
   return (
     <main className="flex flex-row justify-center">
       <section className="flex flex-col">
-      <div className="shadow-2xl border rounded-lg py-2 px-3 m-14 mt-7 text-gray-700 h-52 w-96" style={{ borderBottomColor: "#EE2B7B" }}>
-          hola
+        <div className="shadow border rounded-lg py-2 px-3 m-14 mt-7 text-gray-700 h-52 w-96 grid grid-cols-3 grid-rows-2 gap-4 p-7" style={{ borderBottomColor: "#EE2B7B" }}>
+          <div>01</div>
+          <div className="col-span-2">02</div>
+          <div>03</div>
+          <div className="col-span-2">04</div>
         </div>
-        <div className="shadow-2xl border rounded-lg py-2 px-3 m-14 mt-0 text-gray-700 h-52 w-96" style={{ borderBottomColor: "#EE2B7B" }}>
-          hola
+        <div className="shadow border rounded-lg py-2 px-3 m-14 mt-0 text-gray-700 h-52 w-96 grid grid-cols-3 grid-rows-2 gap-4 p-7" style={{ borderBottomColor: "#EE2B7B" }}>
+          <div>01</div>
+          <div className="col-span-2">02</div>
+          <div>03</div>
+          <div className="col-span-2">04</div>
         </div>
       </section>
       <section className="flex flex-col justify-center">
-        <div className="shadow-2xl border rounded-lg py-2 px-3 m-14 mt-7 text-gray-700 h-52 w-96" style={{ borderBottomColor: "#EE2B7B" }}>
-          hola
+        <div className="shadow border rounded-lg py-2 px-3 m-14 mt-7 text-gray-700 h-52 w-96 grid grid-cols-3 grid-rows-2 gap-4 p-7" style={{ borderBottomColor: "#EE2B7B" }}>
+          <div>01</div>
+          <div className="col-span-2">02</div>
+          <div>03</div>
+          <div className="col-span-2">04</div>
         </div>
-        <div className="shadow-2xl border rounded-lg  py-2 px-3 m-14 mt-0 text-gray-700 h-52  w-96" style={{ borderBottomColor: "#EE2B7B" }}>
-          hola
+        <div className="shadow border rounded-lg  py-2 px-3 m-14 mt-0 text-gray-700 h-52 w-96 grid grid-cols-3 grid-rows-2 gap-4 p-7" style={{ borderBottomColor: "#EE2B7B" }}>
+          <div>01</div>
+          <div className="col-span-2">02</div>
+          <div>03</div>
+          <div className="col-span-2">04</div>
         </div>
       </section>
     </main>
--- src/components/technical_samples/TechnicalMas.jsx
+++ src/components/technical_samples/TechnicalMas.jsx
@@ -17,7 +17,7 @@ export function TechnicalMas() {
       <p className="text-center mt-2"><b>Fecha de selección de la muestra:</b> {sample.fecha}</p>
       <p className="text-center"><b>Diseño muestral:</b> Muestra Aleatoria Simple</p>
       <div className="flex flex-row justify-center">
-        <div className="flex flex-col m-5 max-w-sm min-w-384">
+        <div className="flex flex-col m-5 max-w-sm" style={{ width: 384 }}>
           <p className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mb-2 h-16 pt-3" style={{ borderBottomColor: "#EE2B7B" }}><b>Nombre del auditor:</b><br /> {sample.nombre}</p>
           <p className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mb-2 h-16 text-justify pt-3" style={{ borderBottomColor: "#EE2B7B" }}><b>Correo:</b><br /> {sample.correo}</p>
           <p className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mb-2 h-16 pt-3" style={{ borderBottomColor: "#EE2B7B" }}><b>Nombre de la auditoria:</b><br /> {sample.auditoria}</p>
